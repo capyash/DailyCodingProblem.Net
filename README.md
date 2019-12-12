85890bbe7a16d7fc3d7c2b74b4582e2208de74c7
Yash Capoor <yash.capoor@svam.com>
2019-12-12 16:12:03 +0530

    Basic structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# DailyCodingProblem.Net
+My attempts to solve problems from https://www.dailycodingproblem.com/ in .Net Core
---
+++ YashCapoor.DailyCodingProblem.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace YashCapoor.DailyCodingProblem.ConsoleRunner
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
